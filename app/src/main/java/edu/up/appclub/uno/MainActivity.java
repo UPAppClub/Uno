78a3f0852d4ab14c36d38cbed058843b03ac632e
SBEBS <ericimperio@yahoo.com>
2016-11-02 18:51:15 -0700

    Set up the Animation Surface found in Vegdahl's HW4.

--- app/src/main/java/edu/up/appclub/uno/MainActivity.java
+++ app/src/main/java/edu/up/appclub/uno/MainActivity.java
@@ -3,11 +3,19 @@ package edu.up.appclub.uno;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import edu.up.appclub.uno.animation.AnimationSurface;
+
 public class MainActivity extends AppCompatActivity {
+    private UnoAnimation animator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Connect the animation surface with the animator
+        AnimationSurface mySurface = (AnimationSurface) this.findViewById(R.id.animationSurface);
+        animator = new UnoAnimation();
+        mySurface.setAnimator(animator);
     }
 }
