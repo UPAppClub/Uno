247803b9507ee284eb64c958a9b772dae434ac75
SBEBS <ericimperio@yahoo.com>
2016-11-02 19:00:22 -0700

    Made Card Class

---
+++ app/src/main/java/edu/up/appclub/uno/Card.java
@@ -0,0 +1,17 @@
+package edu.up.appclub.uno;
+
+/**
+ * Created by Eric Imperio on 11/2/2016.
+ */
+
+public class Card {
+    private int value; //0-9,10-skip,11-reverse,12-+2,13-wild, 14-wild+4
+    private char color; //r,g,b,y
+    private static int HEIGHT = 100;
+    private static int WIDTH = 100;
+
+    public Card(int value, char color){
+        this.value = value;
+        this.color = color;
+    }
+}
