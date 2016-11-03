cf5089bbc611c5835ff48c5acc2800b3a0674bcb
noahsperling <sperling19@up.edu>
2016-11-02 19:02:17 -0700

    commit

--- app/src/main/java/edu/up/appclub/uno/MainActivity.java
+++ app/src/main/java/edu/up/appclub/uno/MainActivity.java
@@ -3,11 +3,16 @@ package edu.up.appclub.uno;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class  MainActivity extends AppCompatActivity {
+
+    UnoState u;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        u = new UnoState();
     }
+
+
 }
---
+++ app/src/main/java/edu/up/appclub/uno/UnoState.java
@@ -0,0 +1,35 @@
+package edu.up.appclub.uno;
+
+/**
+ * Created by Noah on 11/2/2016.
+ */
+
+public class UnoState {
+
+    //instance variables
+
+    private ArrayList<Card> drawPile;
+
+    private ArrayList<Card> playedCards;
+
+    private ArrayList<Card> humanPlayerCards;
+
+    private ArrayList<Card> aiPlayerCards;
+
+    private int playerIDs[2];
+
+    private String playerNames[2];
+
+    private int turnID;
+
+    //Getters and Setters
+    public int getTurnID() {
+        return turnID;
+    }
+
+
+
+
+
+
+}
