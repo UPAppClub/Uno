cb26201a3ec16c86c4b70d32459780c8cc815cac
SBEBS <ericimperio@yahoo.com>
2016-11-09 18:48:46 -0800

    worked on UnoState

--- app/src/main/java/edu/up/appclub/uno/Card.java
+++ app/src/main/java/edu/up/appclub/uno/Card.java
@@ -1,17 +1,23 @@
 package edu.up.appclub.uno;
 
+import android.graphics.Canvas;
+
 /**
  * Created by Eric Imperio on 11/2/2016.
  */
 
 public class Card {
     private int value; //0-9,10-skip,11-reverse,12-+2,13-wild, 14-wild+4
-    private char color; //r,g,b,y
+    private char color; //r,g,b,y,w,+ (w = wild, + = wild+4)
     private static int HEIGHT = 100;
     private static int WIDTH = 100;
 
     public Card(int value, char color){
         this.value = value;
         this.color = color;
+    }
+
+    public void onDraw(Canvas c){
+
     }
 }
--- app/src/main/java/edu/up/appclub/uno/UnoState.java
+++ app/src/main/java/edu/up/appclub/uno/UnoState.java
@@ -1,5 +1,6 @@
 package edu.up.appclub.uno;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
@@ -24,14 +25,66 @@ public class UnoState {
 
     private int turnID;
 
-    //Getters and Setters
+    public UnoState(){
+        for(int i = 0; i<15;i++){
+            if(i > 0 && i < 13){
+                drawPile.add(new Card(i, 'r'));
+                drawPile.add(new Card(i, 'y'));
+                drawPile.add(new Card(i, 'g'));
+                drawPile.add(new Card(i, 'b'));
+
+                drawPile.add(new Card(i, 'r'));
+                drawPile.add(new Card(i, 'y'));
+                drawPile.add(new Card(i, 'g'));
+                drawPile.add(new Card(i, 'b'));
+            }else if(i == 0){
+                drawPile.add(new Card(i, 'r'));
+                drawPile.add(new Card(i, 'y'));
+                drawPile.add(new Card(i, 'g'));
+                drawPile.add(new Card(i, 'b'));
+            }else if(i == 13){
+                drawPile.add(new Card(i, 'w'));
+                drawPile.add(new Card(i, 'w'));
+                drawPile.add(new Card(i, 'w'));
+                drawPile.add(new Card(i, 'w'));
+            }else{
+                drawPile.add(new Card(i, '+'));
+                drawPile.add(new Card(i, '+'));
+                drawPile.add(new Card(i, '+'));
+                drawPile.add(new Card(i, '+'));
+            }
+        }
+    }
+
+    //Getters
     public int getTurnID() {
         return turnID;
     }
 
+    public ArrayList<Card> getDrawPile(){ return drawPile; }
 
+    public ArrayList<Card> getPlayedCards(){ return playedCards; }
 
+    public ArrayList<Card> getHumanPlayerCards(){ return humanPlayerCards; }
 
+    public ArrayList<Card> getAiPlayerCards(){ return aiPlayerCards; }
 
+    public int[] getPlayerIDs(){ return playerIDs; }
 
+    public String[] getPlayerNames(){ return playerNames; }
+
+    //Setters
+    public void setTurnID(int turnID){ this.turnID = turnID; }
+
+    public void setDrawPile(ArrayList<Card> drawPile){this.drawPile = drawPile; }
+
+    public void setPlayedCards(ArrayList<Card> playedCards){ this.playedCards = playedCards; }
+
+    public void setAiPlayerCards(ArrayList<Card> aiPlayerCards){ this.aiPlayerCards = aiPlayerCards; }
+
+    public void setHumanPlayerCards(ArrayList<Card> humanPlayerCards){ this.humanPlayerCards = humanPlayerCards; }
+
+    public void setPlayerIDs(int[] playerIDs){ this.playerIDs = playerIDs; }
+
+    public void setPlayerNames(String[] playerNames){ this.playerNames = playerNames; }
 }
